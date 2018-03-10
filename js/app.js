62302a654e6e1472621a785094b2d4a4998214b0
unknown <amierzwinska@gmail.com>
2018-03-10 19:08:46 +0100

    First commit

--- js/app.js
+++ js/app.js
@@ -1,38 +1,170 @@
-/*
- * Create a list that holds all of your cards
- */
+/*  DOM elements */
 
+const cards = Array.from(document.getElementsByClassName('card'));
+const stars = Array.from(document.getElementsByClassName('star'));
+const resetButton = document.getElementsByClassName('restart')[0];
+const popup = document.getElementById('popup');
+const closePopupButton = document.getElementById('closePopup');
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+// cards after shuffling
+let shuffledCards;
+// player move count
+let moves = 0;
+// currently active cards
+let openCards = [];
+// correct quesses counter
+let correctGuesses = [];
 
+// FUNCTIONS
 // Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+const shuffle = (array) => {
+  var currentIndex = array.length,
+    temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-    return array;
+  return array;
 }
 
+// Update moves count
+const movesDisplay = document.getElementsByClassName('moves')[0];
+const updateMoves = () => {
+  movesDisplay.textContent = moves;
+}
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+// Update player star rating view
+
+const updateRating = () => {
+  if (moves > 8) {
+    stars[2].innerHTML = '<i class="far fa-star"></i>';
+  }
+  if (moves > 12) {
+    stars[1].innerHTML = '<i class="far fa-star"></i>';
+  }
+  if (moves > 17) {
+    stars[0].innerHTML = '<i class="far fa-star"></i>';
+  }
+}
+
+// Reset player rating
+const resetRating = () => {
+  stars.forEach(star => star.innerHTML = '<i class="fa fa-star"></i>');
+}
+
+// Shuffle cards and display them
+const shuffleAndDisplayCards = (array) => {
+  array = shuffle(array);
+  // get deck element and add shuffled cards to it
+  const deck = document.getElementsByClassName('deck')[0];
+  let shuffled = '';
+  array.forEach(el => {
+    shuffled += el.outerHTML;
+  });
+  deck.innerHTML = shuffled;
+  // add click listeners to cards
+  shuffledCards = Array.from(document.getElementsByClassName('card'));
+  shuffledCards.forEach(card => card.addEventListener('click', handleCardClick));
+  // keep cards open  for tests
+  // shuffledCards.forEach(card => card.classList.add('show'));
+}
+
+// Reset game
+const resetGame = () => {
+  moves = 0;
+  shuffleAndDisplayCards(cards);
+  resetRating();
+  updateMoves();
+  openCards.length = 0;
+  correctGuesses = 0;
+}
+
+// Reset with confirmation
+const confirmedReset = () => {
+  let confirmation = confirm('Are you sure you want to reset your game?');
+  if (confirmation) {
+    resetGame();
+  }
+}
+
+// Popup functions
+const togglePopup = () => {
+  popup.style.display = 'block';
+}
+const closePopup = () => {
+  popup.style.display = 'none';
+  resetGame();
+}
+
+// Do all the game logic on card click
+const handleCardClick = (e) => {
+  const cardClicked = e.currentTarget;
+  // display card background & symbol
+  cardClicked.classList.add('show', 'open');
+  // cardClicked.classList.add('open');
+  // check if another card open
+  if (openCards.length === 0) {
+    openCards.push(cardClicked);
+    return;
+  }
+  // on two cards clicked
+  if (openCards.length === 1) {
+    shuffledCards.forEach(card => card.removeEventListener('click', handleCardClick));
+    openCards.push(cardClicked);
+    if (openCards[0].isEqualNode(cardClicked)) {
+      console.log('Match!');
+      openCards.forEach(card => {
+        card.classList.add('match')
+      });
+      shuffledCards.forEach(card => {
+        if (!card.classList.contains('match')) {
+          card.addEventListener('click', handleCardClick);
+        }
+      });
+      correctGuesses++;
+      // check for win
+      if (correctGuesses === 3) {
+        console.log('win!');
+        togglePopup();
+        return;
+      }
+      openCards.length = 0;
+      moves++;
+      updateRating();
+      updateMoves();
+    } else {
+      setTimeout(function() {
+        openCards.forEach(card => {
+          card.classList.remove('open', 'show');
+        });
+        openCards.length = 0;
+        moves++;
+        updateRating();
+        updateMoves();
+        shuffledCards.forEach(card => {
+          if (!card.classList.contains('match')) {
+            card.addEventListener('click', handleCardClick);
+          }
+        });
+      }, 600);
+      console.log('No match.');
+    }
+  }
+}
+
+/* Event listeners */
+// reset button
+resetButton.addEventListener('click', confirmedReset);
+// popup button
+closePopupButton.addEventListener('click', closePopup);
+
+
+/* Game play */
+// Start game
+shuffleAndDisplayCards(cards);
+updateMoves();
